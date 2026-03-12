c0845368d26f89009acfdf4fdbef2fa5d9a1e6d6
jayda <J.Souza1@uni.brighton.ac.uk>
2026-03-12 18:27:04 +0000

    commit

--- app/src/main/java/com/example/traveltales/HomePage.java
+++ app/src/main/java/com/example/traveltales/HomePage.java
@@ -1,6 +1,9 @@
 package com.example.traveltales;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,24 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_homepage);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button settingsBtn = findViewById(R.id.settingsBtn);
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, Settings.class);
+                startActivity(intent);
+            }
         });
+
+        Button addBtn = findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, AddNotes.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
